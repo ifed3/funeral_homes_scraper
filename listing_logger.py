dff242bfe93cf13d8133f845a997942643ef858a
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-12-18 14:49:47 -0800

    Change path slashes format

--- listing_logger.py
+++ listing_logger.py
@@ -10,7 +10,7 @@ def create_logger(name):
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     curr_time = datetime.datetime.today().strftime("%m%d%Y %H-%M-%S")
-    file_handler = logging.FileHandler('.\logs\scraper' + ' ' + curr_time + '.log')
+    file_handler = logging.FileHandler('./logs/scraper ' + curr_time + '.log')
     file_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s: %(name)-4s: %(levelname)-4s: %(message)s')
     stream_handler.setFormatter(formatter)
