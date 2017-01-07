d04cf39806e7b7dc6c8cd5144b8df76df4b31a66
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-01-06 17:17:07 -0800

    Handle errors that prevent program from running without interruption

--- home_listings.py
+++ home_listings.py
@@ -13,13 +13,16 @@ def main():
     listings_per_page = sys.argv[2]
     homes_list = []
     # First iteration to retrieve record/pagination info and scrape first page listings
-    page_spider = get_page_spider(url, listings_per_page)
-    collect_listing_objects(page_spider, homes_list)
-    records = set_pagination_data(get_records_spider(page_spider))
-    for num in xrange(records["total_pages"]): # Subsequent iterations with pagination urls
-        paginated_page_spider = get_page_spider(url + '/page/' + str(num+1), listings_per_page)
-        collect_listing_objects(paginated_page_spider, homes_list)
-    output_to_csv(homes_list)
+    try:
+        page_spider = get_page_spider(url, listings_per_page)
+        collect_listing_objects(page_spider, homes_list)
+        records = set_pagination_data(get_records_spider(page_spider))
+        for num in xrange(records["total_pages"]): # Subsequent iterations with pagination urls
+            paginated_page_spider = get_page_spider(url + '/page/' + str(num+1), listings_per_page)
+            collect_listing_objects(paginated_page_spider, homes_list)
+        output_to_csv(homes_list)
+    except:
+        logger.error("Main entry error", exc_info=True)
 
 def collect_listing_objects(page_spider, homes_list):
     """Add listing derived from each page's listings' markup to total list"""
@@ -31,40 +34,72 @@ def set_listing_obj(listing_spider):
     """Collect details to instantiate and set values for a listing object"""
     listing = Listing() # Create a new listing object
     general_class_spider = listing_spider.find(class_="left")
-    info_class_spider = listing_spider.find(class_="right").find(class_="info")
     listing.listing_id = str(listing_spider['id'].split('_')[2])
-    listing.name = str(general_class_spider.find(class_="title").h3.a.string)
+    listing.name = str(general_class_spider.find(class_="title").h3.find_all('a')[-1].string)
     listing.categories = get_listing_categories(general_class_spider.find(class_="title").p)
-    listing.description = str(general_class_spider.p.string)
-    set_listing_address(listing, info_class_spider.address.find_all('span'))
-    set_contact_info(listing, info_class_spider.find_all('p'))
+    try:
+        set_listing_desc(listing, str(general_class_spider.find_all('p')[-1].string))
+    except UnicodeError:
+        set_listing_desc(listing, general_class_spider.find_all('p')[-1].string)
+    try:
+        info_class_spider = listing_spider.find(class_="right").find(class_="info")
+        set_listing_address(listing, info_class_spider.address.find_all('span'))
+        set_contact_info(listing, info_class_spider.find_all('p'))
+    except AttributeError:
+        pass
     return listing
+
+def set_listing_desc(listing, desc_string):
+    """Provide appropriate description"""
+    if desc_string and not "item" in desc_string:
+        listing.description = desc_string
 
 def set_contact_info(listing, contact_spiders):
     """Set phone, fax and website for a listing"""
     for contact_spider in contact_spiders:
-        if contact_spider['class'] == 'claim': # Listings are unverified when they can be claimed
-            listing.verified = False
-        if "phone" in str(contact_spider.strong.string): # Set phone
-            listing.phone = str(contact_spider.find(class_="hide").string)
-        elif "fax" in str(contact_spider.strong.string): # Set fax
-            listing.fax = str(contact_spider.find(class_="hide").string)
-        elif "website" in str(contact_spider.strong.string): # Set website
-            listing.website = str(contact_spider.a['title'])
+        try:
+            if contact_spider['class'] == 'claim':
+                # Listings are unverified when they can be claimed
+                listing.verified = False
+        except KeyError:
+            if "phone" in str(contact_spider.strong.string): # Set phone
+                listing.phone = str(contact_spider.find(class_="hide").string)
+            elif "fax" in str(contact_spider.strong.string): # Set fax
+                listing.fax = str(contact_spider.find(class_="hide").string)
+            elif "website" in str(contact_spider.strong.string): # Set website
+                listing.website = str(contact_spider.a['title'])
 
 def set_listing_address(listing, addr_spans_spider):
     """Set locality and address string for a listing"""
     span_count = len(addr_spans_spider)
     # Span count should be 2, can be 3 when address includes floor or suite
     listing.address = str(addr_spans_spider[0].string).strip()
-    if span_count > 2:
+    if span_count == 1: # Only the city, state and zipcode info is available
+        listing.address = None
+        locality = str(addr_spans_spider[0].string).split(',')
+    elif span_count > 2:
         listing.address += " " + str(addr_spans_spider[1].string).strip()
         locality = str(addr_spans_spider[2].string).split(',')
     else:
         locality = str(addr_spans_spider[1].string).split(',')
-    listing.city = locality[0].strip()
-    listing.state = locality[1].strip()
-    listing.zip = locality[2].strip()
+
+    try:
+        if len(locality) < 2: # Only the zipcode is present
+            listing.city = ""
+            listing.state = ""
+            listing.zip = locality[0].strip()
+        elif len(locality) == 2: # Zipcode is missing
+            listing.city = locality[0].strip()
+            listing.state = locality[1].strip()
+            listing.zip = ""
+        else:
+            listing.city = locality[0].strip()
+            listing.state = locality[1].strip()
+            listing.zip = locality[2].strip()
+    except IndexError:
+        logger.info(locality)
+
+
     if listing.address and listing.city:
         geocode_address(listing)
 
@@ -75,8 +110,12 @@ def geocode_address(listing):
     if location is not None:
         listing.lat = location.lat
         listing.lon = location.lng
-        listing.neighborhood = location.neighborhood
-        listing.county = location.county
+        listing.neighborhood = str(location.neighborhood) if location.neighborhood else None
+        if location.provider == 'bing':
+            county = location.parse['address']['adminDistrict2']
+            listing.county = str(county) if county else None
+        else:
+            listing.county = str(location.county) if location.county else None
 
 def get_listing_categories(category_spider):
     """Return array of categories which listing belongs to"""
